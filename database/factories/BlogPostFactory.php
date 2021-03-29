6537957b66feffce45fe81647603b59bd0c4eb1d
yanochkaku <yanochkaku@gmail.com>
2021-03-29 22:07:04 +0300

    1

---
+++ database/factories/BlogPostFactory.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\BlogPost;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+class BlogPostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = BlogPost::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $title = $this->faker->sentence(rand(3, 8), true);
+        $txt = $this->faker->realText(rand(1000, 4000));
+        $date = $this->faker->dateTimeBetween('-3 months', '-2 months');
+
+        return [
+
+            'category_id'   => rand(1, 11),
+
+            'user_id'       => (rand(1, 2) == 5) ? 1 : 2,
+
+            'title'         => $title,
+
+            'slug'          => Str::slug($title),
+
+            'excerpt'       => $this->faker->text(rand(40, 100)),
+
+            'content_raw'   => $txt,
+
+            'content_html'  => $txt,
+
+            'is_published'  => rand(1, 5) > 1,
+
+            'published_at'  => rand(1, 5) > 1 ? $date : null,
+
+            'created_at'     => $date,
+
+            'updated_at'     => $date,
+
+        ];
+        return [
+            //
+        ];
+    }
+}
---
+++ database/seeders/BlogCategoriesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+class BlogCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            [
+                'name' => 'Невідомий автор',
+                'email' => 'author_unknown@g.g',
+                'password' => bcrypt(Str::random(16)), // password
+            ],
+            [
+                'name' => 'Автор',
+                'email' => 'author1@g.g',
+                'password' => bcrypt('123123'), // password 123123
+            ],
+        ];
+        DB::table('users')->insert($data);
+        //
+    }
+}
