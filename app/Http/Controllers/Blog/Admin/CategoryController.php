fad05cb964733f7ded52f41fac38aa6c6064be66
yanochkaku <yanochkaku@gmail.com>
2021-03-31 20:14:58 +0300

    lab4

---
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Http\Controllers\Blog\Admin;
+
+//use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\BlogCategory;
+use Illuminate\Support\Str;
+
+class CategoryController extends BaseController
+
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $paginator = BlogCategory::paginate(5);
+
+        return view('blog.admin.categories.index', compact('paginator'));
+        //dd(__METHOD__);
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //dd(__METHOD__);
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //dd(__METHOD__);
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //dd(__METHOD__);
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $item = BlogCategory::findOrFail($id);
+        $categoryList = BlogCategory::all();
+
+        return view('blog.admin.categories.edit', compact('item', 'categoryList'));
+        //dd(__METHOD__);
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $item = BlogCategory::find($id);
+        if (empty($item)) { //якщо ід не знайдено
+            return back() //redirect back
+            ->withErrors(['msg' => "Запис id=[{$id}] не знайдено"]) //видати помилку
+            ->withInput(); //повернути дані
+        }
+
+        $data = $request->all(); //отримаємо масив даних, які надійшли з форми
+        if (empty($data['slug'])) { //якщо псевдонім порожній
+            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
+        }
+
+        $result = $item->update($data);  //оновлюємо дані об'єкта і зберігаємо в БД
+
+        if ($result) {
+            return redirect()
+                ->route('blog.admin.categories.edit', $item->id)
+                ->with(['success' => 'Успішно збережено']);
+        } else {
+            return back()
+                ->with(['msg' => 'Помилка збереження'])
+                ->withInput();
+        }
+    }
+        //dd(__METHOD__);
+        //
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //dd(__METHOD__);
+        //
+    }
+}
