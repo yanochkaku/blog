2d7d7c1f622c1673da415033e6546ee18412f99d
yanochkaku <yanochkaku@gmail.com>
2021-04-16 22:48:38 +0300

    lab 9

--- app/Http/Controllers/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Blog/Admin/CategoryController.php
@@ -62,9 +62,7 @@ class CategoryController extends BaseController
     public function store(BlogCategoryCreateRequest $request)
     {
         $data = $request->input(); //отримаємо масив даних, які надійшли з форми
-        if (empty($data['slug'])) { //якщо псевдонім порожній
-            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
-        }
+
 
         $item = (new BlogCategory())->create($data); //створюємо об'єкт і додаємо в БД
 
@@ -135,9 +133,7 @@ class CategoryController extends BaseController
         }
 
         $data = $request->all(); //отримаємо масив даних, які надійшли з форми
-        if (empty($data['slug'])) { //якщо псевдонім порожній
-            $data['slug'] = Str::slug($data['title']); //генеруємо псевдонім
-        }
+
 
         $result = $item->update($data);  //оновлюємо дані об'єкта і зберігаємо в БД
 
