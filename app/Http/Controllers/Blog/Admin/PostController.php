b859b3113351891ae28ddc71fd63abed7e1e99df
yanochkaku <yanochkaku@gmail.com>
2021-04-25 22:57:58 +0300

    lab12

--- app/Http/Controllers/Blog/Admin/PostController.php
+++ app/Http/Controllers/Blog/Admin/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use App\Jobs\BlogPostAfterCreateJob;
+use App\Jobs\BlogPostAfterDeleteJob;
 use App\Http\Requests\BlogPostCreateRequest;
 use App\Repositories\BlogPostRepository;
 use App\Repositories\BlogCategoryRepository;
@@ -68,6 +70,8 @@ class PostController extends BaseController
         $item = (new BlogPost())->create($data); //створюємо об'єкт і додаємо в БД
 
         if ($item) {
+            $job = new BlogPostAfterCreateJob($item);
+            $this->dispatch($job);
             return redirect()
                 ->route('blog.admin.posts.edit', [$item->id])
                 ->with(['success' => 'Успішно збережено']);
@@ -155,9 +159,11 @@ class PostController extends BaseController
             return redirect()
                 ->route('blog.admin.posts.index')
                 ->with(['success' => "Запис id[$id] видалено"]);
+            BlogPostAfterDeleteJob::dispatch($id)->delay(20);
         } else {
             return back()
                 ->withErrors(['msg' => 'Помилка видалення']);
+
         }
 
         //
